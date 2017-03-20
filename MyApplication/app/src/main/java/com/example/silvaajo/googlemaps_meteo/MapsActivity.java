84cd36eb92f9602d0e1142f15fa87e852ae7b6ae
JoaoSilvaAlves <Joao.SilvaAlves@rpn.ch>
2017-03-20 09:58:36 +0100

    Champ entrer ville, marqueur quand appuyer longtemps

--- MyApplication/app/src/main/java/com/example/silvaajo/googlemaps_meteo/MapsActivity.java
+++ MyApplication/app/src/main/java/com/example/silvaajo/googlemaps_meteo/MapsActivity.java
@@ -1,13 +1,24 @@
 package com.example.silvaajo.googlemaps_meteo;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -17,53 +28,80 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    Marker marker;
     @Override
     public void onMapReady(GoogleMap googleMap) {
         m_Map = googleMap;
 
-        // Défini la zone accessible sur la carte, dans ce cas, la Suisse
+
+        // Défini la zone accessible sur la carte, dans ce cas, la Suisse et alentours
         //
         //47.811604, 5.952860            47.816528, 10.514585
         //                      Suisse
         //45.750280, 5.941828            45.800165, 10.518424
         //
         setMapArea(45.750280, 5.941828, 47.816528, 10.514585);
+        moveCameraZoom(46.990125, 6.927998, 15.0f);
         //goToLocationZoom(46.990125, 6.927998, 15);
-    }
-    /*Déplace le centre de la caméra sur les coordonées entrées
-    private void goToLocation(double dbl_lat, double dbl_lng){
-        LatLng latlng_position = new LatLng(dbl_lat, dbl_lng);
-        CameraUpdate camup_update = new CameraUpdateFactory.newLatLng(latlng_position);
-        m_Map.moveCamera(camup_update);
-    }
+        //goToLocation(46.990125, 6.927998);
 
-    //Déplace le centre de la caméra sur les coordonées entrées et fixe le niveau de zoom
-    private void goToLocationZoom(double dbl_lat, double dbl_lng, float f_zoom){
-        LatLng latlng_position = new LatLng(dbl_lat, dbl_lng);
-        CameraUpdate camup_update = new CameraUpdateFactory.newLatLngZoom(latlng_position, f_zoom);
-        m_Map.moveCamera(camup_update);
+        if (m_Map != null){
+            m_Map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(LatLng latLng) {
+
+                }
+            });
+
+            m_Map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener()
+            {
+                @Override
+                public void onMapLongClick(LatLng latlng){
+                    MarkerOptions options = new MarkerOptions().title("Localité").position(new LatLng(46.990125,6.927998)).snippet("I am here");
+                    marker = m_Map.addMarker(options);
+                }
+            });
+        }
     }
-    */
+    private void moveCameraZoom(double dbl_lat, double dbl_lng,float f_zoom)
+    {
+        m_Map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(dbl_lat, dbl_lng), f_zoom));
+    }
 
     //Limite l'accès à la carte Google selon les positions souhaitées. --> Seule la Suisse est entièrement visible
-        private void setMapArea(double dbl_SW_lat, double dbl_SW_lng, double dbl_NE_lat, double dbl_NE_lng)
+    private void setMapArea(double dbl_SW_lat, double dbl_SW_lng, double dbl_NE_lat, double dbl_NE_lng)
     {
         LatLngBounds Switzerland = new LatLngBounds(new LatLng(dbl_SW_lat, dbl_SW_lng), new LatLng(dbl_NE_lat, dbl_NE_lng));
         m_Map.setLatLngBoundsForCameraTarget(Switzerland);
+    }
+
+    public void geoLocate(View view) {
+
+        EditText et = (EditText) findViewById(R.id.editText);
+        String location = et.getText().toString();
+        Geocoder gc = new Geocoder(this);
+
+        try {
+            //Récupère l'adresse d'une ville entrée dans le champ texte
+            List<Address> list = gc.getFromLocationName(location, 1);
+            Address address = list.get(0);
+            final String locality = address.getLocality();
+
+            Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+
+            final double dbl_lat = address.getLatitude();
+            final double dbl_lng = address.getLongitude();
+            moveCameraZoom(dbl_lat, dbl_lng, 15);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
